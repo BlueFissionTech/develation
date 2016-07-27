53210123393c1b1872d5c35f80b77b3c4c2cd854
Devon Scott <Devon Scott>
2016-07-26 20:21:37 -0400

    Addes several behavioral changes. Added Group, and MysqlBulk classes. Updated html rendering classes. Updates to application management classes.

---
+++ src/DevElation/Collections/Group.php
@@ -0,0 +1,46 @@
+<?php
+namespace BlueFission\Collections;
+
+use ArrayAccess;
+use ArrayObject;
+use BlueFission\DevValue;
+use BlueFission\Behavioral\Behaviors\Configurable;
+
+class Group extends Collection implements ICollection, ArrayAccess {
+	public function type( $type = null ) {
+		if ( DevValue::isNull($type) ) {
+			return $this->_type;
+		}
+		$this->_type = $type;
+	}
+
+	private function convert( $value ) {
+		if ( $this->_type && ! $value instanceof $this->_type ) {
+			if ( is_array($value) && is_subclass_of($this->_type, '\BlueFission\Behavioral\Configurable') ) {
+				$object = new $this->_type();
+				$object->assign($value);
+				$value = $object;
+			} elseif ( is_subclass_of($this->_type, '\BlueFission\DevValue') ) {
+				$value = new $this->_type($value);
+			} else {
+				$value = settype($value, $this->_type);
+			}
+		}
+		return $value;
+	}
+
+	public function get( $key ) {
+		$value = parent::get( $key );
+		return $this->convert( $value );
+	}
+
+	public function first()	{
+		$value = parent::first();
+		return $this->convert( $value );
+	}
+
+	public function last() {
+		$value = parent::last();
+		return $this->convert( $value );
+	}
+}
