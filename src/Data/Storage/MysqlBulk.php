675bcbfed2290ec801a214602e67644ee1674bf8
Devon Scott <dscott@bluefission.com>
2020-03-23 22:55:13 -0400

    remove read child from mysqlbulk class

--- src/Data/Storage/MysqlBulk.php
+++ src/Data/Storage/MysqlBulk.php
@@ -15,22 +15,7 @@ class MysqlBulk extends Mysql implements IData
 		$this->_rows->type('\BlueFission\Data\Storage\Mysql');
 		$this->limit(0, 1000);
 	}
-
-	public function read() {
-		parent::read();
-		$res = array();
-		if (method_exists('mysqli_result', 'fetch_all')) # Compatibility layer with PHP < 5.3
-			if ($this->_result)
-				$res = $this->_result->fetch_all( MYSQLI_ASSOC );
-		else {
-			if ($this->_result)
-				for ($res = array(); $tmp = $this->_result->fetch_assoc();) $res[] = $tmp;
-		}
-
-		$this->_rows = new Group( $res );
-		$this->_rows->type('\BlueFission\Data\Storage\Mysql');
-	}
-
+	
 	public function run( $query = "" )
 	{
 		parent::run($query);
