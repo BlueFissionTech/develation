f7732e47c8d8f18f531bda7a8a6548942c40cbd4
Devon Scott <dscott@bluefission.com>
2026-07-04 16:27:50 -0400

    align loader lifecycle patterns

--- tests/Utils/LoaderTest.php
+++ tests/Utils/LoaderTest.php
@@ -7,6 +7,13 @@ use PHPUnit\Framework\TestCase;
 
 class LoaderTest extends TestCase
 {
+    protected function tearDown(): void
+    {
+        $loader = Loader::instance();
+        $loader->config('default_extension', 'php');
+        $loader->config('full_stop', '.');
+    }
+
     public function testInstanceIsSingleton()
     {
         $loader1 = Loader::instance();
@@ -27,9 +34,48 @@ class LoaderTest extends TestCase
         $this->assertEquals('js', $loader->config('default_extension'));
     }
 
+    public function testConfigAcceptsArrayUpdatesForKnownKeys()
+    {
+        $loader = Loader::instance();
+        $loader->config([
+            'default_extension' => 'inc',
+            'full_stop' => ':',
+            'unknown' => 'ignored',
+        ]);
+
+        $this->assertEquals('inc', $loader->config('default_extension'));
+        $this->assertEquals(':', $loader->config('full_stop'));
+        $this->assertNull($loader->config('unknown'));
+    }
+
     public function testLoadClass()
     {
         $loader = Loader::instance();
         $this->assertFalse($loader->load('NotExistingClass'));
+    }
+
+    public function testLoadResolvesConfiguredPath()
+    {
+        $loader = Loader::instance();
+        $dir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid('loader-fixture-', true);
+        $class = 'LoaderFixture' . str_replace('.', '', uniqid('', true));
+
+        mkdir($dir);
+        $file = $dir . DIRECTORY_SEPARATOR . $class . '.php';
+        file_put_contents($file, "<?php class {$class} {}\n");
+
+        try {
+            $loader->addPath($dir);
+            $this->assertNull($loader->load($class));
+            $this->assertTrue(class_exists($class, false));
+        } finally {
+            if (file_exists($file)) {
+                unlink($file);
+            }
+
+            if (is_dir($dir)) {
+                rmdir($dir);
+            }
+        }
     }
 }
